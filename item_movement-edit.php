ff8062bea2161504e472c81d25928b7ced3be5ce
dpobera <oberadaveperol@gmail.com>
2023-04-20 10:07:26 +0800

    item-movement-edit
    done

--- item_movement-edit.php
+++ item_movement-edit.php
@@ -48,9 +48,10 @@ if (isset($_GET['id']) && is_numeric($_GET['id']) && $_GET['id'] > 0) {
             <h1 class="h2 text-secondary font-monospace">ITEM MOVEMENT / Change Records</h1>
             <div class="btn-toolbar mb-2 mb-md-0">
                 <div class="">
-                    <button type="button" class="btn btn-sm btn-secondary" onclick="alert('DI PA AYOS GINAGAWA PA!')">Print Records</button>
-                    <div class="btn-group me-2">
-                    </div>
+                    <form class="" action="php/item_movement-edit-con.php" method="GET">
+                        <button type="submit" class="btn btn-secondary btn-sm bg-gradient" name="update">CHANGE RECORD</button>
+                        <div class="btn-group me-2">
+                        </div>
                 </div>
             </div>
 
@@ -60,32 +61,50 @@ if (isset($_GET['id']) && is_numeric($_GET['id']) && $_GET['id'] > 0) {
 
         <div class="container-fluid shadow-sm bg-light" style="background-color:#ededed;border:1px solid lightgrey;height:65vh;padding:1%">
             <caption>
+
                 <div class="row">
-                    <div class="col">
-                        <label class="text-secondary">PRODUCT ID#</label><input type="text" class="form-control" value="<?php echo str_pad($id, 8, 0, STR_PAD_LEFT) ?>" disabled>
+                    <div class="col-5">
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="floatingInput" value="<?php echo str_pad($id, 8, 0, STR_PAD_LEFT) ?>" disabled>
+                                    <label for="floatingInput">PRODUCT ID#</label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-floating mb-3">
+                                        <input type="text" class="form-control" id="floatingInput" value="<?php echo $product_name ?>" disabled>
+                                        <label for="floatingInput">PRODUCT NAME</label>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
-                    <div class="col">
-                        <label class="text-secondary">PRODUCT NAME</label><input type="text" class="form-control" value="<?php echo $product_name ?>" disabled>
-                    </div>
-                </div>
+
+
+
             </caption><br>
-            <div class="table-responsive shadow-sm" style="height:50vh;overflow-y:suto;overflow-x:auto;background-color:white">
 
-                <?php
-                $servername = "localhost";
-                $username = "root";
-                $password = "";
-                $dbname = "inventorymanagement";
+            <div class="col">
+                <div class="table-responsive shadow-sm p-3" style="height:50vh;overflow-y:suto;overflow-x:auto;background-color:white">
 
-                // Create connection
-                $conn = mysqli_connect($servername, $username, $password, $dbname);
-                // Check connection
-                if (!$conn) {
-                    die("Connection failed: " . mysqli_connect_error());
-                }
+                    <?php
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $dbname = "inventorymanagement";
 
-                $sql = "SELECT move_product.move_id, move_type.mov_type_name, move_product.move_ref, po_tb.po_code, move_product.in_qty, move_product.out_qty, move_product.bal_qty, move_product.mov_date, move_product.mov_type_id, stout_tb.stout_code, stin_tb.stin_code, ep_tb.ep_no, ol_tb.ol_title, rt_tb.rt_no, pinv_tb.pinv_title, order_tb.order_id, order_tb.dr_number,ol_type.ol_type_name,product.product_name
+                    // Create connection
+                    $conn = mysqli_connect($servername, $username, $password, $dbname);
+                    // Check connection
+                    if (!$conn) {
+                        die("Connection failed: " . mysqli_connect_error());
+                    }
+
+                    $sql = "SELECT move_product.move_id, move_type.mov_type_name, move_product.move_ref, po_tb.po_code, move_product.in_qty, move_product.out_qty, move_product.bal_qty, move_product.mov_date, move_product.mov_type_id, stout_tb.stout_code, stin_tb.stin_code, ep_tb.ep_no, ol_tb.ol_title, rt_tb.rt_no, pinv_tb.pinv_title, order_tb.order_id, order_tb.dr_number,ol_type.ol_type_name,product.product_name
                 FROM move_product 
                     INNER JOIN move_type ON move_product.mov_type_id = move_type.mov_type_id
                     INNER JOIN product ON product.product_id = move_product.product_id
@@ -99,66 +118,87 @@ if (isset($_GET['id']) && is_numeric($_GET['id']) && $_GET['id'] > 0) {
                     LEFT JOIN order_tb ON move_product.move_ref = order_tb.order_id
                     LEFT JOIN ol_type ON ol_type.ol_type_id = ol_tb.ol_type_id
                     WHERE move_product.product_id = '$id' ";
-                $result = mysqli_query($conn, $sql);
+                    $result = mysqli_query($conn, $sql);
 
-                if (mysqli_num_rows($result) > 0) {
-                    // output data of each row
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        $moveId = $row['move_id'];
-                        $product_name = $row['product_name'];
-                        $balqty = $row['bal_qty'];
-                        $inqty = $row['in_qty'];
-                        $outqty = $row['out_qty'];
-                        $movid = $row['mov_type_id'];
-                        $movName = $row['mov_type_name'];
-                        $moveref = $row['move_ref'];
+                    if (mysqli_num_rows($result) > 0) {
+                        // output data of each row
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $moveId = $row['move_id'];
+                            $product_name = $row['product_name'];
+                            $balqty = $row['bal_qty'];
+                            $inqty = $row['in_qty'];
+                            $outqty = $row['out_qty'];
+                            $movid = $row['mov_type_id'];
+                            $movName = $row['mov_type_name'];
+                            $moveref = $row['move_ref'];
+                        }
+                    } else {
+                        echo "0 results";
                     }
-                } else {
-                    echo "0 results";
-                }
 
-                mysqli_close($conn);
-                ?>
-
-                <form class="row g-3 p-3" action="php/item_movement-edit-con.php" method="GET">
+                    mysqli_close($conn);
+                    ?>
                     <input type="hidden" name="movId" value="<?php echo $moveId ?>">
+
                     <div class="col">
-                        <label for="inputEmail4" class="form-label">MOVEMENT ID</label>
-                        <input type="text" class="form-control" id="inputEmail4" value="<?php echo $moveId ?>" disabled>
+                        <div class="form-floating mb-2">
+                            <input type="text" class="form-control" id="floatingInput" value="<?php echo str_pad($moveId, 8, 0, STR_PAD_LEFT)  ?>" disabled>
+                            <label for="floatingInput">MOVEMENT ID</label>
+                        </div>
                     </div>
                     <div class="col">
-                        <label for="inputEmail4" class="form-label">PRODUCT ID</label>
-                        <input type="text" class="form-control" id="inputEmail4" value="<?php echo $id ?>" name="prodId">
+                        <div class="form-floating mb-2">
+                            <input type="text" class="form-control" id="floatingInput" value="<?php echo $id ?>" name="prodId">
+                            <label for="floatingInput">PRODUCT ID</label>
+                        </div>
                     </div>
                     <div class="col">
-                        <label for="inputEmail4" class="form-label">BAL QTY</label>
-                        <input type="text" class="form-control" id="inputEmail4" value="<?php echo $balqty ?>" name="balQty">
+                        <div class="form-floating mb-2">
+                            <input type="text" class="form-control" id="floatingInput" value="<?php echo $balqty ?>" name="balQty">
+                            <label for="floatingInput">BALANCE QTY</label>
+                        </div>
                     </div>
                     <div class="col">
-                        <label for="inputEmail4" class="form-label">IN QTY</label>
-                        <input type="text" class="form-control" id="inputEmail4" value="<?php echo $inqty ?>" name="inQty">
+                        <div class="form-floating mb-2">
+                            <input type="text" class="form-control" id="floatingInput" value="<?php echo $inqty ?>" name="inQty">
+                            <label for="floatingInput">IN QTY</label>
+                        </div>
+
                     </div>
                     <div class="col">
-                        <label for="inputEmail4" class="form-label">OUT QTY</label>
-                        <input type="text" class="form-control" id="inputEmail4" value="<?php echo $outqty ?>" name="outQty">
+                        <div class="form-floating mb-2">
+                            <input type="text" class="form-control" id="floatingInput" value="<?php echo $outqty ?>" name="outQty">
+                            <label for="floatingInput">OUT QTY</label>
+                        </div>
                     </div>
                     <div class="col">
-                        <label for="inputEmail4" class="form-label">TYPE</label>
-                        <input type="text" class="form-control" id="inputEmail4" value="<?php echo $movName ?>" disabled>
+                        <div class="form-floating mb-2">
+                            <input type="text" class="form-control" id="floatingInput" value="<?php echo $movName ?>" disabled>
+                            <label for="floatingInput">MOVEMENT TYPE</label>
+                        </div>
                     </div>
                     <div class="col">
-                        <label for="inputEmail4" class="form-label">REFERENCE</label>
-                        <input type="text" class="form-control" id="inputEmail4" value="<?php echo $moveref ?>" name="ref">
+                        <div class="form-floating mb-2">
+                            <input type="text" class="form-control" id="floatingInput" value="<?php echo $moveref ?>" name="ref">
+                            <label for="floatingInput">MOVEMENT REFERENCE</label>
+                        </div>
                     </div>
                     <div class="col-12">
-                        <button type="submit" class="btn btn-secondary" name="update">CHANGE RECORD</button>
+
                     </div>
-                </form>
+                    </form>
+
+                </div>
+
+
+
 
 
             </div>
         </div>
-    </main>
+
+</div>
+</main>
 </div>
 
 <?php include "footer.php" ?>
